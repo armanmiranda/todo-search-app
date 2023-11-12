75814e3bfe47551abaf9d755b515a1a7512ca909
Arman Miranda <arman@localhost.localdomain>
2023-11-12 14:35:27 +0800

    bugfix: fix url for getting todos

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const TODO_API = "https:jsonplaceholder.typicode.com/todos";
+export const TODO_API = "https://jsonplaceholder.typicode.com/todos";
 export const USER_API = "https://jsonplaceholder.typicode.com/users";
