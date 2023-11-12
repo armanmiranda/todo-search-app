8de9a87f6d743472ddeb03b9b1407fa52ceef722
Arman Miranda <arman@localhost.localdomain>
2023-11-12 14:11:53 +0800

    feat: add search functionality

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./searchForm.css";
 
-const SearchBar = () => {
+const SearchBar = ({
+  searchString,
+  handleUpdateSearchString
+}) => {
+  const handleSearch = (event) => {
+    handleUpdateSearchString(event.target.value);
+  }
+
   return(
     <div className='searchForm'>
       <label htmlFor='searchTodo' className='searchForm-label'>
@@ -11,6 +18,9 @@ const SearchBar = () => {
         id='searchTodo'
         name='searchTodo'
         className='searchForm-input'
+        onChange={handleSearch}
+        placeholder="enter keywords to search for"
+        value={searchString}
         type='text' />
     </div>
   )
